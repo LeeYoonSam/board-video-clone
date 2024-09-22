c3fe02a8b8be972081440687c3a77271a9025b39
YS <yoonsam86@gmail.com>
2024-09-22 11:53:37 +0900

    Card Actions - Delete modal 추가

--- README.md
+++ README.md
@@ -280,10 +280,14 @@ export default clerkMiddleware((auth, req) => {
   - 액션 기능 추가
     - Copy board link 
     - Delete board
+      - ConfirmModal 로 모달 띄운 후 액션
 - `app/(dashboard)/_components/board-card/index.tsx` 수정
   - Actions 컴포넌트 추가
 - `convex/board.ts` 수정
   - remove mutation 추가
+- `components/confirm-modal.tsx` 생성
+  - 컨펌 모달 컴포넌트 추가
+
 
 
 ## [Favoriting Functionality](https://www.youtube.com/watch?v=ADJKbuayubE&t=14136s)
---
+++ components/confirm-modal.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+  disabled?: boolean;
+  header: string;
+  descritpion?: string;
+}
+
+export const ConfirmModal = ({
+  children,
+  onConfirm,
+  disabled,
+  header,
+  descritpion,
+}: ConfirmModalProps) => {
+  const handleConfirm = () => {
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {header}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            {descritpion}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={disabled}
+            onClick={handleConfirm}
+          >
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
